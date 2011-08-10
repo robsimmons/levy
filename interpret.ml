ec756dd8c2e4775989328e9016c1a9138f874b63
Robert J. Simmons <robsimmons@gmail.com>
2011-08-10 09:31:26 -0400

    Fixed a bug in the interpreter.
    --HG--
    branch : fast_linear

--- interpret.ml
+++ interpret.ml
@@ -132,7 +132,7 @@ and interp_lin env y = function
       (match (interp env v1), (interp_lin env y v2) with
          | { contents = Zipped ({ contents = Zipper (v1, old_hole) } as r) }, 
            (v2, hole) ->
-           r := Invalid ; hole := !v2 ; (v1, hole)
+           r := Invalid ; old_hole := !v2 ; (v1, hole)
          | { contents = Zipped { contents = Invalid } }, _ ->
            runtime_error "Zipper reused more than once"
 	 | _, _ -> runtime_error "Function expected in application")
